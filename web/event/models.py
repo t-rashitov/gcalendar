af1c21023b55901e653d28da32578cc977a2c744
rashitov.t <devourndevide@gmail.com>
2021-10-01 11:33:49 +0300

    fixed KeyError when event hasn't dateTime, summary fields, added full event update

--- web/event/models.py
+++ web/event/models.py
@@ -25,17 +25,14 @@ class Event(models.Model):
     @classmethod
     def synchronize_with_remote(cls, events: list[dict], user: User) -> None:
         """
-        Создает события из списка параметров, если события с такими event_id не существуют
+        Создает события из списка параметров
         :param events: список параметров событий
         :param user: автор события
         :return:
         """
 
-        current_events_ids = [event['event_id'] for event in events]
-        Event.objects.filter(author=user).exclude(event_id__in=current_events_ids).delete()
-        existing_events_ids = Event.objects.filter(author=user).values_list('event_id', flat=True)
-        new_events = [
-            Event(**params, author=user) for params in events if params['event_id'] not in existing_events_ids]
+        Event.objects.filter(author=user).delete()
+        new_events = [Event(**params, author=user) for params in events]
 
         if new_events:
             Event.objects.bulk_create(new_events)
